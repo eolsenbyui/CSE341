e3666881858142f4f5e642b77ba4bf4541a6c36e
eolsenbyui <eugeneolsen@byui.edu>
2022-09-23 05:57:32 -0400

    Add contacts, dotenv, and body-parser

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', require('./routes'));
+app.use('/contacts', require('./routes/contacts'));
 
 app.listen(port, () => {
-    console.log(`Lesson 2 app running on port ${port}`)
+    console.log(`Lesson 2 app running on port ${port}`);
 })
