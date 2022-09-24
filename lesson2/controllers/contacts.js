e2e8d2380a65215a756677604e4ca8bafeec9c5f
eolsenbyui <eugeneolsen@byui.edu>
2022-09-24 10:57:15 -0400

    Make logging conditional

--- lesson2/controllers/contacts.js
+++ lesson2/controllers/contacts.js
@@ -4,18 +4,23 @@ const db = require('../db');
 displayContacts = async (request, response) => {
     try {
         let id = "";
+        let log = false;
 
         if (request.query.id) id = request.query.id;
         if (request.params.id) id = request.params.id;  // params will take precedence over query if both are present
+        if (request.query.log) {
+            if (request.query.log == "true") log = true;
+        }
 
-        console.log(`ID: \"${id}\"`);
-
-        console.log(`Database name: ${db.getDb().databaseName}`);
+        if (log) {
+            console.log(`ID: \"${id}\"`);
+            console.log(`Database name: ${db.getDb().databaseName}`);
+        }
 
         if (id) {
             const contact = await db.getDb().collection("contacts").findOne({ "_id": new ObjectId(id) });
 
-            console.log(`Contact:\n${JSON.stringify(contact, null, 3)}`);
+            if (log) console.log(`Contact:\n${JSON.stringify(contact, null, 3)}`);
 
             if (contact) {
                 response.send(contact);
@@ -25,7 +30,7 @@ displayContacts = async (request, response) => {
         } else {
             const contacts = await db.getDb().collection("contacts").find().toArray();
 
-            console.log(`Contacts:\n${JSON.stringify(contacts, null, 3)}`);
+            if (log) console.log(`Contacts:\n${JSON.stringify(contacts, null, 3)}`);
 
             response.send(contacts);
         }
