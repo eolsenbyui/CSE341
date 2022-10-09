458e7e33b714143b825803aba9c37267bb5648ae
eolsenbyui <eugeneolsen@byui.edu>
2022-10-08 20:57:02 -0400

    Remove redundant swagger-autogen import

--- lesson4/app.js
+++ lesson4/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const swaggerAutogen = require('swagger-autogen');
 const swaggerUI = require('swagger-ui-express');
 swaggerDocument = require('./swagger.json')
 
