b6064abd4fc4cc66855c7ae18019a0530ef1942d
eolsenbyui <eugeneolsen@byui.edu>
2022-10-08 20:15:07 -0400

    Add Swagger to app

--- lesson4/app.js
+++ lesson4/app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const swaggerAutogen = require('swagger-autogen');
+const swaggerUI = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json')
+
 const db = require('./db');
 
 const port = process.env.PORT || 3000;
@@ -12,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', require('./routes'));
 app.use('/contacts', require('./routes/contacts'));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 db.connect((error, database) => {
     if (error) {
