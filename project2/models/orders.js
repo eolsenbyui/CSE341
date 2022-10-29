f8d9356bf4a19dd914e409c60a2e4fc9463b40e3
eolsenbyui <eugeneolsen@byui.edu>
2022-10-29 16:15:51 -0400

    Add more validation to the Orders schema

--- project2/models/orders.js
+++ project2/models/orders.js
@@ -2,6 +2,15 @@
 
 const { ObjectId } = require("mongodb");
 
+const notEmpty = (items) => {
+    if (items.length === 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 module.exports = (mongoose) => {
     const Order = mongoose.model(
       'order',
@@ -9,12 +18,22 @@ module.exports = (mongoose) => {
             {
                 customerID: {
                     type: ObjectId,
-                    required: [true, 'You must include a customer ID.']
+                    required: [true, 'Each order must include a customer ID.']
                 },
-                items: [{
-                    sku: String,
-                    quantity: Number
-                }],
+                items:  {
+                    [{
+                        sku: {
+                            type: String,
+                            required: [true, 'Each item must have a SKU.']
+                        },
+                        quantity: {
+                            type: Number,
+                            min: [1, 'Each item must have a minimum quantity of 1.']
+                        }
+                    }],
+                    required: true;
+                    validate: [notEmpty, 'The order must include at least one item.']
+                },
                 status: String
             }
         )
