cd7cc29fc90096f8880144fceffba59d2d9595e1
eolsenbyui <eugeneolsen@byui.edu>
2022-11-06 00:01:26 -0400

    Add customers model to mongoose

--- project2/models/index.js
+++ project2/models/index.js
@@ -5,5 +5,6 @@ const db = {};
 db.mongoose = mongoose;
 db.url = process.env.DB_CONNECT;
 db.orders = require('./orders.js')(mongoose);
+db.customers = require('./customers.js')(mongoose);
 
 module.exports = db;
